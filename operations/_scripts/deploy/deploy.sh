67e4864ac3d6a99d6e427b92efbafbf4c7783e20
mcascone <max@bitovi.com>
2023-06-02 15:13:17 -0500

    DRY all paths

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -4,57 +4,60 @@
 # set -x
 set -e
 
+SCRIPTS_PATH="$GITHUB_ACTION_PATH/operations/_scripts"
+OPS_ENV_PATH="$GITHUB_ACTION_PATH/operations/deployment"
+
 echo "::group::In Deploy"
 GITHUB_REPO_NAME=$(echo "$GITHUB_REPOSITORY" | sed 's/^.*\///')
 export GITHUB_REPO_NAME
 
 # Generate buckets identifiers and check them agains Azure Rules 
-TF_STATE_BUCKET="$($GITHUB_ACTION_PATH/operations/_scripts/generate/generate_buckets_identifiers.sh tf | xargs)"
+TF_STATE_BUCKET="$($SCRIPTS_PATH/generate/generate_buckets_identifiers.sh tf | xargs)"
 export TF_STATE_BUCKET
 
-source "./check_bucket_name.sh"
+source "$SCRIPTS_PATH/deploy/check_bucket_name.sh"
 checkStorageName $AZURE_STORAGE_ACCOUNT azure
 checkContainerName $TF_STATE_BUCKET azure
 
-LB_LOGS_BUCKET="$($GITHUB_ACTION_PATH/operations/_scripts/generate/generate_buckets_identifiers.sh lb | xargs)"
+LB_LOGS_BUCKET="$($SCRIPTS_PATH/generate/generate_buckets_identifiers.sh lb | xargs)"
 export LB_LOGS_BUCKET
 
-$GITHUB_ACTION_PATH/operations/_scripts/deploy/check_bucket_name.sh $LB_LOGS_BUCKET
+$SCRIPTS_PATH/deploy/check_bucket_name.sh $LB_LOGS_BUCKET
 
 # Generate subdomain
-$GITHUB_ACTION_PATH/operations/_scripts/generate/generate_subdomain.sh
+$SCRIPTS_PATH/generate/generate_subdomain.sh
 
 # Generate the provider.tf file
-$GITHUB_ACTION_PATH/operations/_scripts/generate/generate_provider.sh
+$SCRIPTS_PATH/generate/generate_provider.sh
 
 # Generate terraform variables
-$GITHUB_ACTION_PATH/operations/_scripts/generate/generate_tf_vars.sh
+$SCRIPTS_PATH/generate/generate_tf_vars.sh
 
 # Generate dot_env
-# $GITHUB_ACTION_PATH/operations/_scripts/generate/generate_dot_env.sh
+# $SCRIPTS_PATH/generate/generate_dot_env.sh
 
 # Generate app repo
-# $GITHUB_ACTION_PATH/operations/_scripts/generate/generate_app_repo.sh
+# $SCRIPTS_PATH/generate/generate_app_repo.sh
 
 # Generate bitops config
-$GITHUB_ACTION_PATH/operations/_scripts/generate/generate_bitops_config.sh
+$SCRIPTS_PATH/generate/generate_bitops_config.sh
 
 # Generate Ansible playbook
-# $GITHUB_ACTION_PATH/operations/_scripts/generate/generate_ansible_playbook.sh
+# $SCRIPTS_PATH/generate/generate_ansible_playbook.sh
 
 # List terraform folder
-# cmd="ls -al $GITHUB_ACTION_PATH/operations/deployment/terraform/"
+# cmd="ls -al $OPS_ENV_PATH/terraform/"
 # echo $cmd && $cmd
 
-cmd="cat $GITHUB_ACTION_PATH/operations/deployment/terraform/provider.tf"
+cmd="cat $OPS_ENV_PATH/terraform/provider.tf"
 echo $cmd && $cmd
 
 # Prints out bitops.config.yaml
-cmd="cat $GITHUB_ACTION_PATH/operations/deployment/terraform/bitops.config.yaml"
+cmd="cat $OPS_ENV_PATH/terraform/bitops.config.yaml"
 echo $cmd && $cmd
 
-# echo "ls GITHUB_ACTION_PATH/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
-# ls "$GITHUB_ACTION_PATH/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
+# echo "ls OPS_ENV_PATH/ansible/app/${GITHUB_REPO_NAME}"
+# ls "$OPS_ENV_PATH/ansible/app/${GITHUB_REPO_NAME}"
 
 TERRAFORM_COMMAND=""
 TERRAFORM_DESTROY=""
