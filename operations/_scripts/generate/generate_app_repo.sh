19b51a92d39278b4912d29bd3556ed719fd9fdf0
mcascone <max@bitovi.com>
2023-06-06 14:09:56 -0500

    ansible wip

--- operations/_scripts/generate/generate_app_repo.sh
+++ operations/_scripts/generate/generate_app_repo.sh
@@ -2,28 +2,32 @@
 
 set -e
 
+echo "In $(basename $0)"
+[[ -n $DEBUG_MODE && $DEBUG_MODE == 'true' ]] && set -x
 
-echo "In generate_app_repo.sh"
-GITHUB_REPO_NAME=$(echo $GITHUB_REPOSITORY | sed 's/^.*\///')
+GITHUB_REPO_NAME=$(echo "$GITHUB_REPOSITORY" | sed 's/^.*\///')
 
-echo "Copying files from GITHUB_WORKSPACE ($GITHUB_WORKSPACE) to ops repo's Ansible deployment (${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME})"
-mkdir -p "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}"
+ANSIBLE_DEPLOYMENT_PATH="$GITHUB_ACTION_PATH/operations/deployment/ansible/app/$GITHUB_REPO_NAME"
+
+echo "Copying files from GITHUB_WORKSPACE '$GITHUB_WORKSPACE' to ops repo's Ansible deployment '$ANSIBLE_DEPLOYMENT_PATH'"
+mkdir -p "$ANSIBLE_DEPLOYMENT_PATH"
 
 TARGET_PATH="$GITHUB_WORKSPACE"
 if [ -n "$APP_DIRECTORY" ]; then
   echo "APP_DIRECTORY: $APP_DIRECTORY"
-  TARGET_PATH="${TARGET_PATH}/${APP_DIRECTORY}"
+  TARGET_PATH="$TARGET_PATH/$APP_DIRECTORY"
 fi
 
 if [ -f "$TARGET_PATH/.gha-ignore" ]; then
-  rsync -a --exclude-from="$TARGET_PATH/.gha-gnore" "$TARGET_PATH"/ "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/"
+  rsync -a --exclude-from="$TARGET_PATH/.gha-gnore" "$TARGET_PATH/" "$ANSIBLE_DEPLOYMENT_PATH/"
 else
-  rsync -a "$TARGET_PATH"/ "${GITHUB_ACTION_PATH}/operations/deployment/ansible/app/${GITHUB_REPO_NAME}/"
+  rsync -a "$TARGET_PATH/" "$ANSIBLE_DEPLOYMENT_PATH/"
 fi
 
-if [ -s "$TARGET_PATH/$REPO_ENV" ]; then
-  echo "Copying checked in env file from repo to Ansible deployment path"
-  cp "$TARGET_PATH/$REPO_ENV" "${GITHUB_ACTION_PATH}/operations/deployment/ansible/repo.env"
-else
-  echo "Checked in env file from repo is empty or couldn't be found"
-fi
+# # check if the dir exists and has a size greater than zero.
+# if [ -s "$TARGET_PATH/$REPO_ENV" ]; then
+#   echo "Copying checked in env file from repo to Ansible deployment path"
+#   cp "$TARGET_PATH/$REPO_ENV" "$GITHUB_ACTION_PATH/operations/deployment/ansible/repo.env"
+# else
+#   echo "Checked in env file from repo is empty or couldn't be found"
+# fi
