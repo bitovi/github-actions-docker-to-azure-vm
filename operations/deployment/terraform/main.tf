d2d1f3a9e54272b61c2fc3a7f5cc2938dd6db69f
mcascone <max@bitovi.com>
2023-06-07 21:49:48 -0500

    test new linux vm resource in new group

--- operations/deployment/terraform/main.tf
+++ operations/deployment/terraform/main.tf
@@ -4,12 +4,12 @@ data "azurerm_resource_group" "test" {
   # tags = local.azure_tags
 }
 
-resource "azurerm_availability_set" "avset" {
-  name                         = "${var.azure_resource_identifier}-AVSet"
-  location                     = data.azurerm_resource_group.test.location
-  resource_group_name          = data.azurerm_resource_group.test.name
-  platform_fault_domain_count  = var.azure_vm_count
-  platform_update_domain_count = var.azure_vm_count
-  managed                      = true
-  # tags = local.azure_tags
-}
+# resource "azurerm_availability_set" "avset" {
+#   name                         = "${var.azure_resource_identifier}-AVSet"
+#   location                     = data.azurerm_resource_group.test.location
+#   resource_group_name          = data.azurerm_resource_group.test.name
+#   platform_fault_domain_count  = var.azure_vm_count
+#   platform_update_domain_count = var.azure_vm_count
+#   managed                      = true
+#   # tags = local.azure_tags
+# }
--- operations/deployment/terraform/network.tf
+++ operations/deployment/terraform/network.tf
@@ -13,31 +13,6 @@ resource "azurerm_subnet" "test" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_public_ip" "test" {
-  name                         = "${var.azure_resource_identifier}-publicIPForLB"
-  location                     = data.azurerm_resource_group.test.location
-  resource_group_name          = data.azurerm_resource_group.test.name
-  allocation_method            = "Static"
-  # tags = local.azure_tags
-}
-
-resource "azurerm_lb" "test" {
-  name                = "${var.azure_resource_identifier}-LB"
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
-  # tags = local.azure_tags
-
-  frontend_ip_configuration {
-    name                 = "${var.azure_resource_identifier}-publicIPAddress"
-    public_ip_address_id = azurerm_public_ip.test.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "test" {
-  loadbalancer_id     = azurerm_lb.test.id
-  name                = "${var.azure_resource_identifier}-BEPool"
-}
-
 resource "azurerm_network_interface" "test" {
   count               = var.azure_vm_count
   name                = "${var.azure_resource_identifier}-NIC-${count.index}"
@@ -51,3 +26,30 @@ resource "azurerm_network_interface" "test" {
   }
   # tags = local.azure_tags
 }
+
+resource "azurerm_public_ip" "test" {
+  name                         = "${var.azure_resource_identifier}-publicIPForLB"
+  location                     = data.azurerm_resource_group.test.location
+  resource_group_name          = data.azurerm_resource_group.test.name
+  allocation_method            = "Static"
+  # tags = local.azure_tags
+}
+
+# resource "azurerm_lb" "test" {
+#   name                = "${var.azure_resource_identifier}-LB"
+#   location            = data.azurerm_resource_group.test.location
+#   resource_group_name = data.azurerm_resource_group.test.name
+#   # tags = local.azure_tags
+
+#   frontend_ip_configuration {
+#     name                 = "${var.azure_resource_identifier}-publicIPAddress"
+#     public_ip_address_id = azurerm_public_ip.test.id
+#   }
+# }
+
+# resource "azurerm_lb_backend_address_pool" "test" {
+#   loadbalancer_id     = azurerm_lb.test.id
+#   name                = "${var.azure_resource_identifier}-BEPool"
+# }
+
+
