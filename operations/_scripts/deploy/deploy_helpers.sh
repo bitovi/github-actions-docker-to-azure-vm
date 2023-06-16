d8f63245388e3c4ca58fe2467ff0a1a43bc5ea71
mcascone <max@bitovi.com>
2023-06-16 15:32:57 -0500

    add isdestroymode

--- operations/_scripts/deploy/deploy_helpers.sh
+++ operations/_scripts/deploy/deploy_helpers.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 function isDebugMode() {
-  if [[ -n $DEBUG_MODE && $DEBUG_MODE == 'true' ]]; then
+  if [[ -n $DEBUG_MODE && $DEBUG_MODE == 'true' ]] || [ $DEBUG_MODE -eq 1 ] ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function isDestroyMode() {
+  if [ "$BITOPS_TERRAFORM_COMMAND" == "destroy" ] || [ "$TERRAFORM_DESTROY" == "true" ]; then
     return 0
   else
     return 1
