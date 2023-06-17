4bd0ac0ff2f6d137ca88d4462084ad3716ffb71a
mcascone <max@bitovi.com>
2023-06-16 21:41:39 -0500

    use isDestroyMode and avoid yoda logic

--- operations/deployment/terraform/bitops.after-deploy.d/destroy_resource_group.sh
+++ operations/deployment/terraform/bitops.after-deploy.d/destroy_resource_group.sh
@@ -5,28 +5,31 @@
 
 set -e
 
+source $BITOPS_TEMPDIR/_scripts/deploy/deploy_helpers.sh
+
 account=$AZURE_STORAGE_ACCOUNT
+group=$azure_resource_identifier
 spacer='================='
 
-if { [ "${BITOPS_TERRAFORM_COMMAND}" != "destroy" ] && [ "${TERRAFORM_DESTROY}" != "true" ]; }; then
+if isDestroyMode; then
+  echo
+  echo $spacer
+  echo "Running $(basename $0) to destroy the resource group $group"
+  source "$BITOPS_TEMPDIR/deployment/_scripts/az_cli_helpers.sh"
+  destroyResourceGroup $group
+
+  echo "Just in case... checking status of storage account $account"
+  if [ "$(storageAccountExists $account)" == "true" ]; then
+    echo "Storage account $account exists. Destroying..."
+    destroyStorageAccount $account $group
+  else
+    echo "Storage account $account does not exist."
+  fi
+  echo $spacer
+else
   echo
   echo $spacer
   echo "Terraform Action is '${BITOPS_TERRAFORM_COMMAND:-$TERRAFORM_DESTROY}'. Skipping $(basename $0)."
   echo $spacer
   exit 0
-else
-  echo
-  echo $spacer
-  echo "Running $(basename $0) to destroy the resource group $azure_resource_identifier"
-  source "$BITOPS_TEMPDIR/deployment/_scripts/az_cli_helpers.sh"
-  destroyResourceGroup $azure_resource_identifier
-
-  echo "Just in case... checking status of storage account $account"
-  if [ "$(storageAccountExists $account)" == "true" ]; then
-    echo "Storage account $account exists. Destroying..."
-    destroyStorageAccount $account $azure_resource_identifier
-  else
-    echo "Storage account $account does not exist."
-  fi
-  echo $spacer
 fi
