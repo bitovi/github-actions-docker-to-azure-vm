33d6435f48395b1f57062fbc403ea3aa4651752d
mcascone <max@bitovi.com>
2023-06-19 10:26:34 -0500

    path names

--- operations/_scripts/deploy/deploy.sh
+++ operations/_scripts/deploy/deploy.sh
@@ -6,17 +6,17 @@ echo "::group::Deploy"
 
 pwd
 
-source "./deploy_helpers.sh"
+SCRIPTS_PATH='operations/_scripts'
+DEPLOY_SCRIPTS_PATH="$SCRIPTS_PATH/deploy"
+GENERATE_SCRIPTS_PATH="$SCRIPTS_PATH/generate"
+
+source "$DEPLOY_SCRIPTS_PATH/deploy_helpers.sh"
 source "$GENERATE_SCRIPTS_PATH/generate_helpers.sh"
 isDebugMode && set -x
 set -e
 
-SCRIPTS_PATH="$BITOPS_TEMP_DIR/_scripts"
-DEPLOY_SCRIPTS_PATH="$SCRIPTS_PATH/deploy"
-GENERATE_SCRIPTS_PATH="$SCRIPTS_PATH/generate"
-
 OPS_REPO_ENV_NAME="deployment"
-OPS_ENV_PATH="$BITOPS_TEMP_DIR/$OPS_REPO_ENV_NAME"
+OPS_ENV_PATH="operations/$OPS_REPO_ENV_NAME"
 OPS_REPO_ANSIBLE_PATH="$OPS_ENV_PATH/ansible"
 OPS_REPO_TERRAFORM_PATH="$OPS_ENV_PATH/terraform"
 
